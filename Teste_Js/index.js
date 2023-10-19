b9aeca49cfb427997c09eba6bd1f0b95ad271348
leort11 <leonardo.figueiredo31@hotmail.com>
2023-10-18 22:42:17 -0300

    Adicionando mais testes

--- Teste_Js/index.js
+++ Teste_Js/index.js
@@ -1,12 +1,13 @@
+const vet = []
 
 function verNum() {
     let num = document.getElementById('fnum').value;
 
-    if (num%2 == 0) {
-        document.getElementById('result').innerHTML = "Resultado: " + num +" É par"
+    if (num % 2 == 0) {
+        document.getElementById('result').innerHTML = "Resultado: " + num + " É par"
 
     } else {
-        document.getElementById('result').innerHTML = "Resultado: " + num +" Não é par"
+        document.getElementById('result').innerHTML = "Resultado: " + num + " Não é par"
 
     }
 }
@@ -18,10 +19,36 @@ function alerta() {
 
 function dataHora() {
     const data = new Date()
-    
+
     const dia = data.getDate()
     const mes = data.getMonth()
     const ano = data.getFullYear()
 
     document.getElementById('demo').innerHTML = `Hoje é ${dia} / ${mes} / ${ano}`
+}
+
+let teste = () => {
+    let obj = document.getElementById('callAlert').value
+
+
+    switch (obj) {
+        case "a":
+            vet.push("a")
+            document.getElementById('demo2').innerHTML = "Lista: " + vet
+            break
+
+        case "b":
+            vet.push("b")
+            document.getElementById('demo2').innerHTML = "Lista: " + vet
+            break
+
+        case "c":
+            vet.push("c")
+            document.getElementById('demo2').innerHTML = "Lista: " + vet
+            break
+
+        default:
+            alert("Letra inválida")
+
+    }
 }
